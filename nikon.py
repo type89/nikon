501deb7dd7c240568bc819930c68ed4502a1e189
type89 <zong1yuan2@gmail.com>
2018-11-19 22:37:05 +0900

    フォルダ名、ファイル名の変数修正

--- nikon.py
+++ nikon.py
@@ -2,9 +2,13 @@ import subprocess
 import time
 import datetime
 
+foldername = "/home/pi/nikon/"
+
 for i in range (3):
     now = datetime.datetime.today()
     strdate = str(now.date()) + "_" + str(now.time())
-    cmd ="gphoto2 --capture-image-and-download --filename /home/pi/nikon/D40_" + strdate + ".jpg"
+    filename = "D40_" + strdate + ".jpg"
+
+    cmd ="gphoto2 --capture-image-and-download --filename " + folder + filename
     subprocess.call(cmd.split())
     time.sleep(5)
